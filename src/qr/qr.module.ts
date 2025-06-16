5558174e684b3d529448c1bb2f78c74ac5c8d853
Precious Enuagwune <76190326+PreciousIfeaka@users.noreply.github.com>
2025-06-16 14:04:59 +0100

    Update qr.module.ts

--- src/qr/qr.module.ts
+++ src/qr/qr.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { MovieModule } from "../movie/movie.module";
 import { QRController } from "./qr.controller";
 import { QRService } from "./qr.service";
+import { ConfigModule } from "@nestjs/config";
+
 
 @Module({
-  imports: [MovieModule],
+  imports: [MovieModule, ConfigModule],
   controllers: [QRController],
   providers: [QRService]
 })

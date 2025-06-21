d9350ca911171e7aebc14cf7d4913397a21d3374
PreciousIfeaka <preciousifeaka@gmail.com>
2025-06-21 14:06:25 +0100

    chore: refactored deploy configs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { Request, Response } from 'express';
 
-async function bootstrap() {
+
+async function bootstrap(): Promise<INestApplication> {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
@@ -9,5 +12,12 @@ async function bootstrap() {
     credentials: true,
   });
   await app.listen(3000);
+
+  return app;
 }
-bootstrap();
+
+export default async(req: Request, res: Response) => {
+  const app = await bootstrap();
+  const expressApp = app.getHttpAdapter().getInstance();
+  return expressApp(req, res);
+};
